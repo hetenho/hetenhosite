1b62ed74d932a345038c7451218444b2f65f149e
Henrik Tenhovirta <hetenhosc@gmail.com>
2024-04-11 13:52:34 +0300

    fix asset path

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,3 +1,3 @@
 export default function Icon({ name, height = 16 }: { name: string, height?: number }) {
-  return <img src={`/public/${name}.svg`} alt="github" width={height} height={height} />
+  return <img src={`/${name}.svg`} alt="github" width={height} height={height} />
 }
